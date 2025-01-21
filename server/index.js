ccd24d8757c5cc37713ba3eb1dc3989e2b4b4fdc
Jackshan-Venujan <venujanfast@gmail.com>
2025-01-22 02:12:06 +0530

    backend part finished

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const connection = require('./db');
+const userRoutes = require('./routes/user');
+const authRoutes = require('./routes/auth');
 
 //database connection
 connection();
@@ -12,6 +14,10 @@ connection();
 //middleware
 app.use(express.json());
 app.use(cors());
+
+//routes
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
